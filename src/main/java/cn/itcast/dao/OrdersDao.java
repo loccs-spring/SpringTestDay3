a53fbd0c6542866708969a5cfb2f017e4adc8bce
spring <liyong2003@gmail.com>
2017-04-10 11:37:10 +0800

    Project Init
    Project Init

---
+++ src/main/java/cn/itcast/dao/OrdersDao.java
@@ -0,0 +1,8 @@
+package cn.itcast.dao;
+
+/**
+ * Created by spring on 2017/4/10.
+ */
+public class OrdersDao {
+
+}
---
+++ src/main/java/cn/itcast/service/OrdersService.java
@@ -0,0 +1,9 @@
+package cn.itcast.service;
+
+/**
+ * Created by spring on 2017/4/10.
+ */
+
+
+public class OrdersService {
+}
